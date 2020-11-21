f117fe39a176ef3c6082e9aaedca858c70f6f10e
Guilherme Alves <policarto@96>
2020-11-20 22:08:53 -0300

    Tornando a classe Funcionario abstrata.

--- src/eti/policarto/heranca/Funcionario.java
+++ src/eti/policarto/heranca/Funcionario.java
@@ -1,6 +1,6 @@
 package eti.policarto.heranca;
 
-public class Funcionario {
+public abstract class Funcionario {
     private String nome;
     private String cpf;
     private double salario;
--- src/eti/policarto/heranca/TesteFuncionario.java
+++ src/eti/policarto/heranca/TesteFuncionario.java
@@ -2,12 +2,12 @@ package eti.policarto.heranca;
 
 public class TesteFuncionario {
     public static void main(String[] args) {
-        Funcionario funcionario = new Funcionario();
-        funcionario.setNome("Guilherme Alves");
-        funcionario.setCpf("13445634589");
-        funcionario.setSalario(5000.0);
+        Funcionario gerente = new Gerente();
+        gerente.setNome("Guilherme Alves");
+        gerente.setCpf("13445634589");
+        gerente.setSalario(5000.0);
 
-        System.out.println(funcionario.getNome());
-        System.out.println(funcionario.getBonificacao());
+        System.out.println(gerente.getNome());
+        System.out.println(gerente.getBonificacao());
     }
 }
