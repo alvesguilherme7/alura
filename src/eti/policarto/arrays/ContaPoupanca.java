b00b26c14feb7b49b57ab557d0caa883de8e11f0
Guilherme Alves <policarto@96>
2020-11-26 07:15:44 -0300

    Adicionando exemplos de array e linked.

---
+++ src/eti/policarto/arrays/ContaPoupanca.java
@@ -0,0 +1,12 @@
+package eti.policarto.arrays;
+
+public class ContaPoupanca extends Conta {
+    public ContaPoupanca(int agencia, int numero) {
+        super(agencia, numero);
+    }
+
+    @Override
+    public void deposita(double valor) {
+        super.saldo += valor;
+    }
+}
