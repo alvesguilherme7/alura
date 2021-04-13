efae35f41b758c3c6adaf6a4162481dfb4298cff
guilherme.alves <policarto@96>
2021-04-13 07:43:37 -0300

    Adicionando exemplos de uso da classe Constructor para construir novos objetos.

--- src/main/java/eti/policarto/reflection/geral/model/Controle.java
+++ src/main/java/eti/policarto/reflection/geral/model/Controle.java
@@ -3,8 +3,25 @@ package eti.policarto.reflection.geral.model;
 import eti.policarto.reflection.geral.commons.Run;
 
 public class Controle implements Run {
+
+    private String msg;
+
+    public Controle() {
+        this.msg = "Eu sou o construtor padrão.";
+    }
+
+    public Controle(String msg) {
+        this.msg += "Eu sou o contrutor com uma String.";
+        this.msg += msg == null ?  "": msg;
+    }
+
+    private Controle(String msg, Integer teste) {
+        this.msg = "Eu sou o contrutor com uma String e um Integer.";
+        this.msg += msg == null ?  "": msg;
+    }
+
     public void print(){
-        System.out.println("Eu sou o controle");
+        System.out.println(this.msg);
     }
 
     public void print(String msg){
--- src/main/java/eti/policarto/reflection/geral/reflexao/ObtendoClass.java
+++ src/main/java/eti/policarto/reflection/geral/reflexao/ObtendoClass.java
@@ -9,28 +9,37 @@ public class ObtendoClass {
     public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         // 3 formas de obter o objeto Class parametrizado
         Class<Controle> controleClass1 = Controle.class;
-
         Class<? extends Controle> controleClass2 = new Controle().getClass();
-
         Class<?> controleClass3 = Class.forName("eti.policarto.reflection.geral.model.Controle");
 
         // Depois disso, você pode usar de forma parametrizada ou não, dependendo do passo anterior
-
         // parametrizado retorna o objeto Controle diretamente
-        Controle controleParametrizado1  = controleClass1.newInstance();
-        Controle controle = controleClass2.newInstance();
-        Object   controleNaoParametrizdo = controleClass3.newInstance();
+        Controle parametrizado1  = controleClass1.newInstance();
+        Controle parametrizado2  = controleClass2.newInstance();
+        Object   generico1       = controleClass3.newInstance();
 
-        Controle controleParametrizado3;
-        if(controleNaoParametrizdo instanceof Controle) controleParametrizado3 = (Controle) controleNaoParametrizdo;
+        Controle parametrizado3;
+        if(generico1 instanceof Controle) parametrizado3 = (Controle) generico1;
 
-        controleParametrizado1.print(" Esse metódo pertence a classe controle. ");
+        parametrizado1.print(" Esse metódo pertence a classe controle. ");
 
-        // apartir do Java 9, a forma correta de se instanciar e invocar metódos é
-        Constructor<Controle> constructors = controleClass1.getConstructor();
-        System.out.println(constructors);
-        Controle instancia = constructors.newInstance();
-        System.out.println(instancia);
-        instancia.print(" Invocando metódo da instância recém criada. ");
+        // Apartir do Java 9, a forma correta de se construir um objeto e invocar metódos é através do Constructor.
+        // Ele consegue avisar de possíveis exceções em tempo de escrita/compilação.
+        Constructor<Controle> constructor1 = controleClass1.getConstructor();
+        // defina os tipos dos parametros da sobrecarga desejada
+        Constructor<Controle> constructor2 = controleClass1.getConstructor(String.class);
+
+        // O getDeclaredConstructor é capaz de buscar construtores privados
+        Constructor<Controle> constructor3 = controleClass1.getDeclaredConstructor(String.class, Integer.class);
+        Constructor<?>[] arrayConstructos4 = controleClass1.getDeclaredConstructors();
+        for (Constructor<?> c : arrayConstructos4) {
+            System.out.println("Contrutor: "+c.getModifiers()+" - "+c.getName());
+        }
+
+        Controle controle1 = constructor1.newInstance();
+        Controle controle2 = constructor2.newInstance("Mensagem Incial.");
+        controle1.print();
+        controle2.print();
+
     }
 }
